8a57882379fbd883e9744f767bdfdcea5b07787a
fullere <fullere@hartwick.edu>
2020-04-07 22:31:40 -0400

    wrote code

--- PalindromeCheck.py
+++ PalindromeCheck.py
@@ -1,3 +1,45 @@
 # Elizabeth Fuller
 # 4/2/2020
 # Palindrome Check
+
+def check(string):
+    length = len(string)
+    if length == 1 or length == 0:
+        return True
+    else:
+        if string[0] == " ":
+            string = string.replace(string[0],'')
+        if string[-1] == " ":
+            string = string.replace(string[-1],'')
+        if string[0] != string[-1]:
+            return False
+        else:
+            check(string)
+
+
+
+def compare(string):
+    if string[0] == " ":
+        string = string.replace(string[0],'')
+    if string[-1] == " ":
+        string = string.replace(string[-1],'')
+    if string[0] != string[-1]:
+        return False
+    else:
+        length(string)
+
+
+
+def prepare(string):
+    string = string.strip()
+    string = string.lower()
+    return string
+
+print("Enter string to check for palindrome:")
+userString = input("> ")
+userString = prepare(userString)
+pali = check(userString)
+print(pali)
+
+
+#print(check(newo))
